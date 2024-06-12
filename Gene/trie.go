c3132f604ca6b6cc50addb80ab698a5c3f603f83
iWorld-y <caima12138@foxmail.com>
2024-06-12 19:29:17 +0800

    feat: Trie

---
+++ Gene/trie.go
@@ -0,0 +1,29 @@
+package Gene
+
+type node struct {
+	pattern  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+// 第一个匹配成功的节点
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// 所有匹配成功的节点
+func (n *node) matchChildren(part string) []*node {
+	var nodes []*node
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
