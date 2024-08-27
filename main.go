a1c5be3c8d1a9cd9c28e38487c41e2b11a31ded6
iWorld-y <caima12138@foxmail.com>
2024-08-27 22:56:27 +0800

    feat: basic handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path: %q\n", req.URL.Path)
+}
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q]: %q\n", k, v)
+	}
 }
